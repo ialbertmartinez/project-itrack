2b2f486efb42134331150388941afea247e750a7
Albert Martinez <ialbertmartinez@outlook.com>
2021-05-27 22:15:13 -0700

    added modal functionality

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function(){
+	$("#addItem").click(function(){
+		$("#itemModal").modal("show");
+	});
+
+	$("#addLocation").click(function(){
+		$("#locationModal").modal("show");
+	});
+	$("#addCategory").click(function(){
+		$("#categoryModal").modal("show");
+	});
+
+});
